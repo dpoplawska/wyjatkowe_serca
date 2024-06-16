635e965a83ba42cd95eac7e4559b112b07d247a4
Diana Popławska <poplawska97@gmail.com>
2024-06-16 23:03:14 +0200

    more footer work

--- frontend/app/src/sections/Footer.js
+++ frontend/app/src/sections/Footer.js
@@ -5,7 +5,7 @@ import PlaceIcon from '@mui/icons-material/Place';
 
 export default function Footer() {
     return (
-        <footer className="footer mt-auto py-3 bg-dark text-white">
+        <footer className="footer mt-auto py-0 mt-3 bg-light">
             <div className="footer">
                 <div className="row">
                     <div className="col-12 col-sm-4 mb-3 mb-md-0">
@@ -15,8 +15,13 @@ export default function Footer() {
                         <h5>Kontakt</h5>
                         {/* <p>Phone: +48 123 456 789</p> */}
                         {/* <p>Email: kontakt@example.com</p> */}
-                        <p><PlaceIcon /> Al. Jerozolimskie 123A
-                            02-017 Warszawa</p>
+                        <p className="contact">
+                            <a href="https://www.google.com/maps/place/al.+Jerozolimskie+123A,+02-017+Warszawa/@52.2249832,20.988533,17z/data=!3m1!4b1!4m6!3m5!1s0x471ecc912dcfbc11:0x914920fa7b955f73!8m2!3d52.2249832!4d20.9911079!16s%2Fg%2F11bw3wtd2n?entry=ttu" target="_blank" rel="noopener noreferrer">
+                                <PlaceIcon id="addressIcon" />
+                                Al. Jerozolimskie 123A
+                                02-017 Warszawa
+                            </a>
+                        </p>
 
                     </div>
                     <div className="col-12 col-sm-4 mb-3 mb-md-0">
